92c773e40891b3020ed62cece69d9ce90cceb9b2
Zack Ormond <zack_ormond@gmail.com>
2017-03-24 11:33:37 -0600

    Fill first alert with api call

--- theJourney/public/javaScript/moveShip.js
+++ theJourney/public/javaScript/moveShip.js
@@ -22,48 +22,25 @@ $(document).ready(function(){
             html: 'you selected ' + result,
         };
     }
-    var text1 ="<span>This is the question</span><br><span>1. Answer</span><br><span>2. Answer</span><br><span>3. Answer</span>";
-    var options1 = {
+
+    var getSwalHtml = function(data){
+        return  "<span>"+data.situation +"</span><br>" + 
+                "<span>" +data.answers[0].answerText + "</span><br>" +
+                "<span>"+data.answers[1].answerText + "</span><br>" + 
+                "<span>" +data.answers[2].answerText + "</span>";
+    }
+
+
+    var options = {
         option1: "Option 1",
         option2: "Option 2",
         option3: "Option 3"
     };
-    var options2 =  {
-        'option2-1': "Option 2-1",
-        'option2-2': "Option 2-2",
-        'option2-3': "Option 2-3"
-    };
 
-    var options3 = {
-        'option3-1': "Option 3-1",
-        'option3-2': "Option 3-2",
-        'option3-3': "Option 3-3"
-    };
-    var options4 = {
-        'option4-1': "Option 4-1",
-        'option4-2': "Option 4-2",
-        'option4-3': "Option 4-3"
-    };;
-    var options5 = {
-        'option5-1': "Option 5-1",
-        'option5-2': "Option 5-2",
-        'option5-3': "Option 5-3"
-    };;
-    var options6 = {
-        'option6-1': "Option 6-1",
-        'option6-2': "Option 6-2",
-        'option6-3': "Option 6-3"
-    };; 
-    var options7 = {
-        'option7-1': "Option 7-1",
-        'option7-2': "Option 7-2",
-        'option7-3': "Option 7-3"
-    };
-
-    var getSwalStop = function(name,options){
+    var getSwalStop = function(name, situation){
         return{
             title:name,
-            html: text1,
+            html: situation,
             input:"radio",
             inputOptions:options,
             background: '#332106 url(../oldPaper.jpg)',
@@ -112,68 +89,69 @@ swal({
             {
                 endPointX = wW * .07; endPointY =  wH * .40; angle1 = 0; angle2 = 0;
                 myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear', function(){//first stop
-                    $("#seaWaves")[0].pause();
-                    swal(getSwalStop("The Lotus Eaters",options1)).then(function(result){
-                        swal(onwardSwal(result)).then(function(){
-                                endPointX = wW * .12; endPointY = wH * .63; angle1 = 10; angle2 = 17;
-                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear', function()//second stop
-                                {
-                                    $("#seaWaves")[0].pause();  
-                                    swal(getSwalStop("The Cyclops",options2)).then(function(result){
-                                        swal(onwardSwal(result)).then(function(){
-                                        endPointX = wW * .09; endPointY = wH * .15; angle1 = 45; angle2 = 329;
-                                        myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//third stop
-                                        {
-                                            $("#seaWaves")[0].pause();
-                                            swal(getSwalStop("The Lyc",options3)).then(function(result){
-                                                swal(onwardSwal(result)).then(function(){
-                                                    endPointX = wW * .16; endPointY = wH * .10; angle1 = 343; angle2 = 30;
-                                                    myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//fourth stop
-                                                    {
-                                                        $("#seaWaves")[0].pause();  
-                                                        swal(getSwalStop("Hades",options4)).then(function(result){
-                                                            swal(onwardSwal(result)).then(function(){
-                                                                endPointX = wW * .25; endPointY = wH * .35; angle1 = 33; angle2 = 315;
-                                                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//fifth stop
-                                                                {
-                                                                    $("#seaWaves")[0].pause();
-                                                                    swal(getSwalStop("The Sirens",options5)).then(function(result){
-                                                                        swal(onwardSwal(result)).then(function(){
-                                                                            endPointX = wW * .37; endPointY = wH * .63; angle1 = 326.565; angle2 = 47.452;
-                                                                            myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//sixth stop
-                                                                            {
-                        
-                                                                                endPointX = wW * .45; endPointY = wH * .34; angle1 =44; angle2 = 317;
-                                                                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//final stop
+                    $.getJSON('/situations',function(data){
+                        console.log(data);
+                        swal(getSwalStop(data.title,getSwalHtml(data))).then(function(result){
+                            swal(onwardSwal(result)).then(function(){
+                                    endPointX = wW * .12; endPointY = wH * .63; angle1 = 10; angle2 = 17;
+                                    myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear', function()//second stop
+                                    {
+                                        $("#seaWaves")[0].pause();  
+                                        swal(getSwalStop("The Cyclops",getSwalHtml(data))).then(function(result){
+                                            swal(onwardSwal(result)).then(function(){
+                                            endPointX = wW * .09; endPointY = wH * .15; angle1 = 45; angle2 = 329;
+                                            myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//third stop
+                                            {
+                                                $("#seaWaves")[0].pause();
+                                                swal(getSwalStop("The Lyc",getSwalHtml(data))).then(function(result){
+                                                    swal(onwardSwal(result)).then(function(){
+                                                        endPointX = wW * .16; endPointY = wH * .10; angle1 = 343; angle2 = 30;
+                                                        myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//fourth stop
+                                                        {
+                                                            $("#seaWaves")[0].pause();  
+                                                            swal(getSwalStop("Hades",getSwalHtml(data))).then(function(result){
+                                                                swal(onwardSwal(result)).then(function(){
+                                                                    endPointX = wW * .25; endPointY = wH * .35; angle1 = 33; angle2 = 315;
+                                                                    myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//fifth stop
+                                                                    {
+                                                                        $("#seaWaves")[0].pause();
+                                                                        swal(getSwalStop("The Sirens",getSwalHtml(data))).then(function(result){
+                                                                            swal(onwardSwal(result)).then(function(){
+                                                                                endPointX = wW * .37; endPointY = wH * .63; angle1 = 326.565; angle2 = 47.452;
+                                                                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//sixth stop
                                                                                 {
-                                                                                    $("#seaWaves")[0].pause();
-                                                                                    swal(getSwalStop("Calypso",options6)).then(function(result){
-                                                                                        swal(onwardSwal(result)).then(function()
-                                                                                        {
-                                                                                            endPointX = wW * .55; endPointY = wH * .50; angle1 =44; angle2 = 317; 
-                                                                                            myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function(){
-                                                                                                
+                            
+                                                                                    endPointX = wW * .45; endPointY = wH * .34; angle1 =44; angle2 = 317;
+                                                                                    myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//final stop
+                                                                                    {
+                                                                                        $("#seaWaves")[0].pause();
+                                                                                        swal(getSwalStop("Calypso",getSwalHtml(data))).then(function(result){
+                                                                                            swal(onwardSwal(result)).then(function()
+                                                                                            {
+                                                                                                endPointX = wW * .55; endPointY = wH * .50; angle1 =44; angle2 = 317; 
+                                                                                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function(){
+                                                                                                    
+                                                                                                });
                                                                                             });
                                                                                         });
                                                                                     });
-                                                                                });
-                                                                            });                                                                                                                      
-                                                                        }); 
-                                                                    });
-                                                                });                                                        
-                                                            });
-                                                        })
-                                                    });                                                   
-                                                });
+                                                                                });                                                                                                                      
+                                                                            }); 
+                                                                        });
+                                                                    });                                                        
+                                                                });
+                                                            })
+                                                        });                                                   
+                                                    });
+                                                });  
                                             });  
-                                        });  
+                                        });
                                     });
                                 });
                             });
                         });
                     });
                 });
-
             });
         });
     });   
--- theJourney/routes/index.js
+++ theJourney/routes/index.js
@@ -1,5 +1,24 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost/OdysseusDB');
+
+var situationSchema = mongoose.Schema({
+  title: String,
+  situation: String,
+  answers:[{
+    answerText: String,
+    answerScore: String,
+  }]
+});
+
+var situation = mongoose.model('situations', situationSchema);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error: '));
+db.once('open', function(){
+    console.log("MongoDB connected");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,8 +29,15 @@ router.post('/answer', function(req,res,next){
 
 });
 
-router.get('/question', function(req,res,next){
-
+router.get('/situations', function(req,res,next){
+  console.log("In the situations route");
+  situation.find(function(err,questions){
+    if(err) return console.error(err);
+    else{
+      console.log(questions);
+      res.json(questions[0]);
+    }
+  });
 });
 
 module.exports = router;
