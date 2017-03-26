8f091ec8665b3d67bc35f498e9b3e9d7661dc1a4
Zack Ormond <zack_ormond@gmail.com>
2017-03-26 14:56:35 -0600

    uncouple ship movements

--- theJourney/public/javaScript/moveShip.js
+++ theJourney/public/javaScript/moveShip.js
@@ -1,13 +1,29 @@
 $(document).ready(function(){
+    $.ajax({
+        url:"/situations",
+        dataType:"application/json",
+        success:onDataRecieved
+    })
+});
+
+function onDataRecieved(response) {
+    var questions = JSON.parse(response);
+    console.log(questions);
+    var options = {
+        option1: "Option 1",
+        option2: "Option 2",
+        option3: "Option 3"
+    };
+
     var myShip = $("#ship");
     var endPointX = 0;
     var endPointY = 0;
     var angle1 = 0;
     var angle2 = 0;
+    var stopCount = 0;
     var wH = $(window).height();
     var wW = $(window).width();
-    ///Widely used values
-    var validate = 
+    var validate =
         function (result) {
             return new Promise(function (resolve, reject) {
                 if (result) {
@@ -17,34 +33,44 @@ $(document).ready(function(){
                 }
             })
         };
-    //Swal to make post request
+
+        var currScore = 0;
+
     var onwardSwal = function(result){
+        var display;
+        if (result === "option1"){
+          display = questions[stopCount].answers[0].answerText;
+          //currScore += parseInt(questions[stopCount].answers[0].answerScore);
+        } 
+        else if(result ==="option2")
+        {
+          display = questions[stopCount].answers[1].answerText;
+          //currScore += parseInt(questions[stopCount].answers[1].answerScore);
+        }
+        else{
+          display = questions[stopCount].answers[2].answerText;
+          //currScore += parseInt(questions[stopCount].answers[2].answerScore);
+        }
         return {
             title:'Onward!',
-            html: 'you selected ' + result,
+            html: 'You selected: ' + display,
+            background: '#332106 url(../oldPaper.jpg)',
         };
     }
 
-    //Help used to create html for that will be presented in the swal. Data will be retrieved from the database.
-    var getSwalHtml = function(data){
-        return  "<span>"+data.situation +"</span><br>" + 
-                "<span>" +data.answers[0].answerText + "</span><br>" +
-                "<span>"+data.answers[1].answerText + "</span><br>" + 
-                "<span>" +data.answers[2].answerText + "</span>";
+    var finalswal = function(result)
+    {
+        return {
+            title:'Results: ',
+            //html: "You scored " + currScore + "/35",
+            background: '#332106 url(../oldPaper.jpg)',
+        };
     }
 
-    //Radio button options
-    var options = {
-        option1: "Option 1",
-        option2: "Option 2",
-        option3: "Option 3"
-    };
-
-    //Standard swal at each stop in the journey
-    var getSwalStop = function(name, situation){
+    var getSwalStop = function(name){
         return{
             title:name,
-            html: situation,
+            html: createQuestion(questions[stopCount]),
             input:"radio",
             inputOptions:options,
             background: '#332106 url(../oldPaper.jpg)',
@@ -56,121 +82,142 @@ $(document).ready(function(){
         }
     }
 
-    //Helper function to create the path that the ship travels.
     var createParam = function(X, Y, a1,a2){
         bezier_params = {
-            start: { 
-                x: $("#ship").position().left, 
-                y: $("#ship").position().top, 
+            start: {
+                x: $("#ship").position().left,
+                y: $("#ship").position().top,
                 angle: a1
-            },  
-            end: { 
+            },
+            end: {
                 x: X,
-                y: Y, 
+                y: Y,
                 angle: a2,
                 length: 1
                 }
-        } 
+        }
         return bezier_params;
     }
 
- //This is the start of all the animations.    
+
+
+    var createQuestion = function(curQ){
+        return "<span>"+ curQ.situation +"</span><br><br><span>Option 1: " +curQ.answers[0].answerText +
+               "</span><br><br><span>Option 2: " +curQ.answers[1].answerText + "</span><br><br><span>Option 3: "+curQ.answers[2].answerText+"</span>"
+    }
+
 swal({
         title:"Welcome to Odysseus' Journey!",
-        text: "These are the directions",
+        text: "The Trojan War has just ended, now you need to get home.",
         background: '#332106 url(../oldPaper.jpg)',
         width: '75%',
         padding: 75,
         allowOutsideClick: false,
         allowEscapeKey: false
     }).then(function(){
-        $("#seaWaves")[0].play();
         endPointX = wW * .60;
         endPointY = wH * .65;
         angle2 = 40;
-        myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2)),},1500, 'linear',function(e) 
+        myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2)),},150, 'linear',startVoyage);
+    });
+
+    var startVoyage = function()  {
+        endPointX = wW * .25; endPointY =  wH * .65; angle1 = 0; angle2 = 0;
+        myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 150,'linear', function()
         {
-            endPointX = wW * .25; endPointY =  wH * .65; angle1 = 0; angle2 = 0;
-            myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500,'linear', function()
+            endPointX = wW * .10; endPointY =  wH * .40; angle1 = 0; angle2 = 0;
+            myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 150, 'linear', firstStop);
+
+        });
+    }
+
+    var firstStop = function() {
+        $("#seaWaves")[0].pause();
+        swal(getSwalStop("The Lotus Eaters")).then(function(result){
+            swal(onwardSwal(result)).then(function(){
+                stopCount++;
+                console.log("Score after first stop: " + currScore);
+                endPointX = wW * .15; endPointY = wH * .60; angle1 = 10; angle2 = 17;
+                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear', secondStop);
+            });
+        });
+    }
+
+    var secondStop = function() {
+        $("#seaWaves")[0].pause();
+        swal(getSwalStop("The Cyclops")).then(function(result){
+            swal(onwardSwal(result)).then(function(){
+                stopCount++;
+                  console.log("Score after second stop: " + currScore);
+                endPointX = wW * .11; endPointY = wH * .15; angle1 = 45; angle2 = 329;
+                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',thirdStop);
+            });
+        });
+    }
+
+    var thirdStop = function() {
+        $("#seaWaves")[0].pause();
+        swal(getSwalStop("The Lyc")).then(function(result){
+           
+            swal(onwardSwal(result)).then(function(){
+                stopCount++;
+                console.log("Score after third stop: " + currScore);
+                endPointX = wW * .16; endPointY = wH * .10; angle1 = 343; angle2 = 30;
+                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',fourthStop);
+            });
+        });
+    }
+
+    var fourthStop = function() {
+        $("#seaWaves")[0].pause();
+        swal(getSwalStop("Hades")).then(function(result){
+        
+            swal(onwardSwal(result)).then(function(){
+                stopCount++;
+                console.log("Score after fourth stop: " + currScore);
+                endPointX = wW * .25; endPointY = wH * .35; angle1 = 33; angle2 = 315;
+                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',fifthStop);
+            });
+        })
+    }
+
+    var fifthStop = function() {
+        $("#seaWaves")[0].pause();
+        swal(getSwalStop("The Sirens")).then(function(result){
+      
+            swal(onwardSwal(result)).then(function(){
+                stopCount++;
+                console.log("Score after fifth stop: " + currScore);
+                endPointX = wW * .37; endPointY = wH * .63; angle1 = 326.565; angle2 = 47.452;
+                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',sixthStop);
+            });
+        });
+    }
+
+    var sixthStop = function() {
+        endPointX = wW * .45; endPointY = wH * .34; angle1 =44; angle2 = 317;
+        myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',finalStop);
+    }
+
+    var finalStop = function() {
+        $("#seaWaves")[0].pause();
+        swal(getSwalStop("Calypso")).then(function(result){
+            swal(onwardSwal(result)).then(function()
             {
-                endPointX = wW * .07; endPointY =  wH * .40; angle1 = 0; angle2 = 0;
-                myShip.animate({path : new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear', function()
-                {//first stop
-                    $.getJSON('/situations',function(data)
+                stopCount++;
+                console.log("Score after sixth stop: " + currScore);
+                endPointX = wW * .55; endPointY = wH * .50; angle1 =44; angle2 = 317;
+                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function(){
+                    swal(getSwalStop("Ithaca")).then(function(result)
                     {
-                        console.log(data);
-                        swal(getSwalStop(data.title,getSwalHtml(data))).then(function(result)
+                        swal(onwardSwal(result)).then(function()
                         {
-                            swal(onwardSwal(result)).then(function()
-                            {
-                                endPointX = wW * .12; endPointY = wH * .63; angle1 = 10; angle2 = 17;
-                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear', function()//second stop
-                                {
-                                    $("#seaWaves")[0].pause();  
-                                    swal(getSwalStop("The Cyclops",getSwalHtml(data))).then(function(result)
-                                    {
-                                        swal(onwardSwal(result)).then(function()
-                                        {
-                                            endPointX = wW * .09; endPointY = wH * .15; angle1 = 45; angle2 = 329;
-                                            myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//third stop
-                                            {
-                                                $("#seaWaves")[0].pause();
-                                                swal(getSwalStop("The Lyc",getSwalHtml(data))).then(function(result)
-                                                {
-                                                    swal(onwardSwal(result)).then(function(){
-                                                        endPointX = wW * .16; endPointY = wH * .10; angle1 = 343; angle2 = 30;
-                                                        myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//fourth stop
-                                                        {
-                                                            $("#seaWaves")[0].pause();  
-                                                            swal(getSwalStop("Hades",getSwalHtml(data))).then(function(result)
-                                                            {
-                                                                swal(onwardSwal(result)).then(function(){
-                                                                    endPointX = wW * .25; endPointY = wH * .35; angle1 = 33; angle2 = 315;
-                                                                    myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//fifth stop
-                                                                    {
-                                                                        $("#seaWaves")[0].pause();
-                                                                        swal(getSwalStop("The Sirens",getSwalHtml(data))).then(function(result)
-                                                                        {
-                                                                            swal(onwardSwal(result)).then(function()
-                                                                            {
-                                                                                endPointX = wW * .37; endPointY = wH * .63; angle1 = 326.565; angle2 = 47.452;
-                                                                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//sixth stop
-                                                                                {
-                            
-                                                                                    endPointX = wW * .45; endPointY = wH * .34; angle1 =44; angle2 = 317;
-                                                                                    myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()//final stop
-                                                                                    {
-                                                                                        $("#seaWaves")[0].pause();
-                                                                                        swal(getSwalStop("Calypso",getSwalHtml(data))).then(function(result)
-                                                                                        {
-                                                                                            swal(onwardSwal(result)).then(function()
-                                                                                            {
-                                                                                                endPointX = wW * .55; endPointY = wH * .50; angle1 =44; angle2 = 317; 
-                                                                                                myShip.animate({path: new $.path.bezier(createParam(endPointX,endPointY,angle1,angle2))}, 1500, 'linear',function()
-                                                                                                {
-                                                                                                    
-                                                                                                });
-                                                                                            });
-                                                                                        });
-                                                                                    });
-                                                                                });                                                                                                                      
-                                                                            }); 
-                                                                        });
-                                                                    });                                                        
-                                                                });
-                                                            })
-                                                        });                                                   
-                                                    });
-                                                });  
-                                            });  
-                                        });
-                                    });
-                                });
-                            });
+                            console.log("Score after seventh stop: " + currScore);
+                            swal(finalswal());
                         });
                     });
                 });
             });
         });
-    });   
-});
+    }
+}
--- theJourney/routes/index.js
+++ theJourney/routes/index.js
@@ -5,107 +5,118 @@ var router = express.Router();
 mongoose.connect('mongodb://localhost/OdysseusDB');
 
 var situations = [{
+    _id: 0,
     title: "The Lotus Eaters",
-    situation: "Situation: After a terrible storm created by Zeus, you make your way to the Land of the Lotus Eaters. You send 3 men to go greet" +
+    situation: "After a terrible storm created by Zeus, you make your way to the Land of the Lotus Eaters. You send 3 men to go greet" +
                "these foreign people. Upon return, you find that these men have no wanting to go home anymore. What do you do? ",
     answers:[{
       answerText: "Force the men on the ship, no matter how much they cry.",
-      answerScore: "1"
+      answerScore: "1" //Odyssian
     },{
       answerText: "Kill these men for their mutiny and abandonement.",
-      answerScore: "0"
+      answerScore: "0" //Brutal
     },{
       answerText: "Leave the men on the island to live onward in ignorant bliss.",
-      answerScore: "2"
+      answerScore: "-1" //Different
     }]
   },{
+    _id: 1,
     title: "Polyphemus",
-    situation: " Situation: After escaping the spellbinding effect of the Lotus Eaters you landed near the island of the Cyclopes. You decided" +
+    situation: "After escaping the spellbinding effect of the Lotus Eaters you landed near the island of the Cyclopes. You decided" +
                "to go exploring where the Cyclopes lives and end up being captured by Polyphemus. Within 12 hours of meeting Polyphemus he eats" +
                "four of your men. The need to escape is dire. How do you do so? ",
     answers:[{
-      answerText: "Engage in hand to hand combat with Polyphemus.",
-      answerScore: "2"
+      answerText: "Engage in hand to hand combat with Polyphemus. The other giants here the conflict, opening the giant door, and allowing you to escape.",
+      answerScore: "2" //Different
     },{
-      answerText: "Somehow sweet talk into sacrificing all of your crew in the cave to Polyphemus in return for only your escape.",
-      answerScore: "0"
+      answerText: "Somehow sweet talk Polyphemus into sacrificing all of your crew in the cave to Polyphemus in return for only your escape.",
+      answerScore: "0" //Brutal
     },{
-      answerText: "Fashion a giant shiv and stab him in the eye.",
-      answerScore: "1"
+      answerText: "Fashion a giant shiv and stab him in the eye which leads Polyphemus to open the door, leading to your escape.",
+      answerScore: "1" //Odyssian
     }]
   },{
+    _id: 2,
     title: "Laistrygones",
     situation: "After almost making it home, your men open the bag of winds and blow you back. You end up in the land of the Laistrygonians." +
                "You disembark and send 3 men to find the inhabitants. These men go and implore a young woman to tell them of the king of the land." +
                "To their horror the leader takes one of them and makes dinner of him. As you try to escape with your men...",
     answers:[{
-      answerText: "Stay and fight until all your men are back on their ships.",
-      answerScore: "2"
+      answerText: "You throw some of your own men down to slow the oncoming Laistrygonians, easily making back to your own ship in time.",
+      answerScore: "0" //Brutal
     },{
-      answerText: "Cut your own ship free and flee as fast as you can.",
-      answerScore: "1"
+      answerText: "You Cut your own ship free and flee as fast as you can, only worrying about the men on your ship.",
+      answerScore: "1" //Odyssian
     },{
-      answerText: "Run towards the Laistrygones as a distraction to save your men.",
-      answerScore: "2"
+      answerText: "You run towards the Laistrygones as a distraction to save your men. You still luckily make it back to your ship in time.",
+      answerScore: "-1" //Different
     }]
   },{
+    _id: 3,
     title: "Hades",
-    situation: "Unfortunetaly only your ship escape the Laistrygonians onslaught of boulder throwing. After saving your men from being pigs the rest of their lives from" + 
+    situation: "Unfortunetaly only your ship escapes the Laistrygonians onslaught of boulder throwing. After saving your men from being pigs the rest of their lives from" + 
                "the witch Circe, she tells you that you must go to Hades to and speak to Tiresias to find your way home. You rally your men and follow the instructions Circe" +
                "gave you to reach the underworld. After performing the rituals, who do you choose to speak to? ",
     answers:[{
       answerText: "Elpenor, Tiresias, Agamemnon, Achilles, Ajax Odyssian",
-      answerScore: "TODO"
+      answerScore: "1" //Odyssian
     },{
       answerText: "Only talk to Tiresias to receive the prophecy and leave.",
-      answerScore: "TODO"
+      answerScore: "0" //Brutal
     },{
       answerText: "Speak with everyone that approaches.",
-      answerScore: "TODO"
+      answerScore: "-1" //Different
     }]
   },{//This one has two answers.
+    _id: 4,
     title: "The Sirens",
     situation: "On your way back to Ithaca you must pass the deadly sirens. You have the option to stuff everyone's ears with wax including your own, or you can have" + 
                "you men tie you up while you listen. Which do you choose?",
     answers:[{
-      answerText: "Stuff everyone's ears with wax",
-      answerScore: "TODO"
+      answerText: "Stuff everyone's ears with wax, including your own",
+      answerScore: "-1"//Different
     },{
       answerText: "Stuff everyone's ears but your own.",
-      answerScore: "TODO"
-    },]
+      answerScore: "1" //Odyssian
+    },{
+      answerText: "Lead an onslaught against the Sirens, losing half of your men.",
+      answerScore: "0"//Brutal
+    }]
   },{
+    _id: 5,
     title: "Calypso",
     situation: "After passing the Sirens and Scylla and Charybdis, your men disobey you and slaughter Helio's cattle. You and your men are punished by Zeus with a great" +
                "storm in which only you survive and wash up on Calypso's island. Calypso is quite into you... what do you do?",
     answers:[{
-      answerText: "Live on your own and wait to see if the God's will help you.",
-      answerScore: "TODO"
+      answerText: "Live on your own, remembering that you have a wife in Ithica, and wait to see if the gods will help you.",
+      answerScore: "-1"//Different
     },{
-      answerText: "Resist but give in and enjoy your stay until the God's 'save' you.",
-      answerScore: "TODO"
+      answerText: "Resist but give in and enjoy your stay until the gods 'save' you.",
+      answerScore: "1" //Odyssian
     },{
       answerText: "Welcome Calypso into your life and enjoy your stay until the God's 'save' you.",
-      answerScore: "TODO"
+      answerScore: "0" //Brutal
     }]
   },{
+    _id: 6,
     title: "Ithica",
     situation: "You have returned to Ithaca after the God's decided to convince Calypso to let you return home. You disguised yourself to see who has been loyal to you"+
                "all this time. After passing Penolope's test, what do you decide to do with the suitors?",
     answers:[{
       answerText: "Slaughter them all, showing no mercy.",
-      answerScore: "TODO"
+      answerScore: "1" //Odyssian
     },{
       answerText: "Let the suitors live, but force them from your home.",
-      answerScore: "TODO"
+      answerScore: "-1" //different
     },{
       answerText: "Banish the suitors from Ithaca, making sure that they never have any influence ever again.",
-      answerScore: "TODO"
+      answerScore: "-1" //different
     }]
   }];
 
 
 var situationSchema = mongoose.Schema({
+  _id: Number,
   title: String,
   situation: String,
   answers:[{
@@ -141,8 +152,8 @@ var clearSituations = function(){
     });
   }
 }
-clearSituations();
-initializeSituations();
+//clearSituations();
+//initializeSituations();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile('theJourney.html', { root : 'public' });
@@ -154,11 +165,11 @@ router.post('/situations', function(req,res,next){
 
 router.get('/situations', function(req,res,next){
   console.log("In the situations route");
-  situation.find(function(err,questions){
+  situation.find({}).sort({'_id': '1'}).exec(function(err,Situations){
     if(err) return console.error(err);
     else{
-      console.log(questions);
-      res.json(questions[0]);
+      console.log(Situations);
+      res.json(Situations);
     }
   });
 });
